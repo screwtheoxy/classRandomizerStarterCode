b53fac68238726d9f7eff8c00147d35404151609
Thomas Cross <thomas.corey.cross@gmail.com>
2017-03-01 23:45:23 +0000

    Class randomizer

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
+var code2CollegeStudents = ["Basil", "Thomas", "Alexis", "Abi", "Isaac", 
+"Evila", "Spencer", "Garrett", "Tumolac", "Tim", "George", "Adrian"];
 
+var code2CollegeEducators = ["Sam", "Sam", "Dr. Dre", "Matt"];
 
+$("#studentButton").click(function(){
+$("#studentDisplay").html(code2CollegeStudents[Math.floor(Math.random()*code2CollegeStudents.length)]);
+});
 
+$("#teacherButton").click(function(){
+$("#teacherDisplay").html(code2CollegeEducators[Math.floor(Math.random()*code2CollegeEducators.length)]);
+});
+
+//text box and button to add student to array
+$("#putInAStudent").click(function(){
+    code2CollegeStudents.push($("#grabThisStudent").val());
+    $("#addStudent").append("You have successfully added " + ($("#grabThisStudent").val()) + "</br>");
+});
+
+$("#putInATeacher").click(function(){
+    code2CollegeEducators.push($("#grabThisTeacher").val());
+    $("#addTeacher").append("You have successfully added " + ($("#grabThisTeacher").val()) + "</br>");
 });
